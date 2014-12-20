1edca60b339b0dc37a801166dc2895f67c03f895
Montwulf <montwulf@gmail.com>
2014-12-20 11:55:33 +0100

    - Skrypt z bazą danych

---
+++ SQL/dna base.sql
@@ -0,0 +1,17 @@
+/*------------ DROPY
+DROP TABLE User;
+*/
+use DNA;
+CREATE TABLE "User"
+(
+	"Id" INT IDENTITY(1,1) PRIMARY KEY,
+	"Login" VARCHAR(30) NOT NULL UNIQUE,
+	"Password" VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE "Friend"
+(
+	"Id" INT IDENTITY(1,1) PRIMARY KEY,
+	"OwnerId" int FOREIGN KEY REFERENCES "User"(Id),
+	"FriendId" int FOREIGN KEY REFERENCES "User"(Id)
+);
