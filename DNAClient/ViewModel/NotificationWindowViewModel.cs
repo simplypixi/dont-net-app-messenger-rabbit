6fbb56365489e841bdf0a2b200f900650a13143b
simplypixi <pixiproject@gmail.com>
2015-01-07 20:11:17 +0100

    Poprawki + notyfikacje

--- DNAClient/ViewModel/NotificationWindowViewModel.cs
+++ DNAClient/ViewModel/NotificationWindowViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 
 namespace DNAClient.ViewModel
@@ -23,6 +25,8 @@ namespace DNAClient.ViewModel
     public class NotificationWindowViewModel : ViewModelBase
     {
         private string message;
+        private BasicDeliverEventArgs messageTMP;
+        private string sender;
         public string Message
         {
             get
@@ -40,7 +44,7 @@ namespace DNAClient.ViewModel
         {
         }
 
-        public NotificationWindowViewModel(string sender, string type)
+        public NotificationWindowViewModel(string sender, BasicDeliverEventArgs mess, string type)
         {
             this.CloseWindowCommand = new RelayCommand(this.CloseWindow);
 
@@ -50,7 +54,20 @@ namespace DNAClient.ViewModel
                 this.Message = sender + " przesyła wiadomość...";
             if(type == "file")
                 this.Message = sender + " przesyła plik...";
+
+            this.messageTMP = mess;
+            this.sender = sender;
+            this.NewConversationWindowCommand = new RelayCommand(this.NewConversationWindow);
         }
+
+        public RelayCommand NewConversationWindowCommand { get; set; }
+        private void NewConversationWindow(object parameter)
+        {
+            ConversationViewModel cvModel = ProductionWindowFactory.CreateConversationWindow(sender);
+            GlobalsParameters.openWindows.Add(cvModel);
+            cvModel.Receive(this.messageTMP);
+        }
+        
 
         public RelayCommand CloseWindowCommand { get; set; }
 
