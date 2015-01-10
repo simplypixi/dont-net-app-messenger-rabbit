6d84950deab3edaecfffca2f303a69653e8e2fb6
Montwulf <montwulf@gmail.com>
2015-01-10 12:17:04 +0100

    - Dodawanie/ Usuwanie znajomych z listy kontaktów - Ustawienie case sensitive na hasła w zapytaniach sql

--- DNA/DatabaseHelper.cs
+++ DNA/DatabaseHelper.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
 
 namespace DNA
 {
@@ -11,18 +13,18 @@ namespace DNA
     {
         private SqlConnection conn;
         private string connectionString;
-        
+
         public DatabaseHelper()
         {
-            this.connectionString = @"Data Source=.;Initial Catalog=DNA; User Id=dna; Password=dna;";
+            this.connectionString = @"Data Source=montwulfpc\Baza;Initial Catalog=DNA; User Id=dna; Password=dna;";
         }
 
         public bool Login(string login, string password)
         {
-            using(conn = new SqlConnection(this.connectionString))
+            using (conn = new SqlConnection(this.connectionString))
             {
                 conn.Open();
-                String query = string.Format("SELECT 1 FROM [DNA].[dbo].[User] WHERE Login = '{0}' AND Password = '{1}'", login, password);
+                String query = string.Format("SELECT 1 FROM [DNA].[dbo].[User] WHERE Login = '{0}' AND Password COLLATE Polish_CS_AS = '{1}'", login, password);
                 using (SqlCommand command = new SqlCommand(query, conn))
                 {
                     // TODO: DELETE THIS LINE
@@ -44,6 +46,11 @@ namespace DNA
 
         public bool Register(string login, string password)
         {
+            if (this.IsUserExist(login))
+            {
+                return false;
+            }
+
             using (conn = new SqlConnection(this.connectionString))
             {
                 conn.Open();
@@ -53,7 +60,7 @@ namespace DNA
                     // TODO: DELETE THIS LINE
                     Console.WriteLine(command.CommandText);
 
-                    if(command.ExecuteNonQuery() > 0 )
+                    if (command.ExecuteNonQuery() > 0)
                     {
                         return true;
                     }
@@ -62,6 +69,116 @@ namespace DNA
                         return false;
                     }
                 }
+            }
+        }
+
+        public bool AddFriend(string ownerLogin, string friendLogin)
+        {
+            int? ownerId, friendId;
+            ownerId = this.GetUserIdWithLogin(ownerLogin);
+            friendId = this.GetUserIdWithLogin(friendLogin);
+
+            if (ownerId == null || friendId == null)
+            {
+                return false;
+            }
+
+            using (conn = new SqlConnection(this.connectionString))
+            {
+                conn.Open();
+                String query = string.Format("INSERT INTO [DNA].[dbo].[Friend] (OwnerId, FriendId) VALUES('{0}', '{1}')", ownerId, friendId);
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    // TODO: DELETE THIS LINE
+                    Console.WriteLine(command.CommandText);
+
+                    if (command.ExecuteNonQuery() > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        public bool RemoveFriend(string ownerLogin, string friendLogin)
+        {
+            int? ownerId, friendId;
+            ownerId = this.GetUserIdWithLogin(ownerLogin);
+            friendId = this.GetUserIdWithLogin(friendLogin);
+
+            if (ownerId == null || friendId == null)
+            {
+                return false;
+            }
+
+            using (conn = new SqlConnection(this.connectionString))
+            {
+                conn.Open();
+                String query = string.Format("DELETE FROM [DNA].[dbo].[Friend] WHERE OwnerId = '{0}' AND FriendId = '{1}'", ownerId, friendId);
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    // TODO: DELETE THIS LINE
+                    Console.WriteLine(command.CommandText);
+
+                    if (command.ExecuteNonQuery() > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        private int? GetUserIdWithLogin(string login)
+        {
+            using (conn = new SqlConnection(this.connectionString))
+            {
+                conn.Open();
+                String query = string.Format("SELECT Id FROM User WHERE Login = '{0}'", login);
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    // TODO: DELETE THIS LINE
+                    Console.WriteLine(command.CommandText);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return reader.GetInt32(1);
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        private bool IsUserExist(string login)
+        {
+            using (conn = new SqlConnection(this.connectionString))
+            {
+                conn.Open();
+                String query = string.Format("SELECT 1 FROM User WHERE Login = '{0}'", login);
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    // TODO: DELETE THIS LINE
+                    Console.WriteLine(command.CommandText);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
             }
         }
 
